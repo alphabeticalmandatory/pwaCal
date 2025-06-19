f62a91a104f98dbfc0187b70c2d965bc131ba89f
alphabeticalmandatory <ilovebulkhead+git27feb@gmail.com>
2025-06-19 20:40:06 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,21 @@
-const CACHE_NAME = 'material-calendar-v1';
-const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles/main.css',
-    '/scripts/app.js',
-    '/scripts/calendar.js',
-    '/scripts/storage.js',
-    '/scripts/alarms.js',
-    '/assets/icons/icon-192x192.png',
-    '/assets/icons/icon-512x512.png',
-    'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS_TO_CACHE))
-    );
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("pwa-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/b.html",
+        "/manifest.json",
+        "/1.png",
+        "/2.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('notificationclick', event => {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(windowClients => {
-            for (const client of windowClients) {
-                if (client.url === '/' && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            if (clients.openWindow) {
-                return clients.openWindow('/');
-            }
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
