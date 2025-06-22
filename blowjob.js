e3b3bcc8f25c7e19f15291fedfb39352f3159646
alphabeticalmandatory <ilovebulkhead+git27feb@gmail.com>
2025-06-23 03:33:53 +0530

    Update blowjob.js

--- blowjob.js
+++ blowjob.js
@@ -1,12 +1,34 @@
-self.addEventListener('install', event => {
-  alert('Service worker installing...');
+const CACHE_NAME = 'pwa-cache-v1';
+const URLS_TO_CACHE = ['index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  alert('Service worker activating...');
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('index.html');
+    })
+  );
 });
